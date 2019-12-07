9d40390094f625a4b11c3202859a97b8de5fa65e
LeloAK <kojtekarek@gmail.com>
2019-12-07 12:00:28 +0100

    cookies i edycja uzytkownikow

--- additionalPHP/admincontrolbaredit.php
+++ additionalPHP/admincontrolbaredit.php
@@ -5,8 +5,13 @@
 	$stmt = $pdo -> prepare('UPDATE user SET user_verifyEmail = :type WHERE user_id = :id;');
 	$stmt -> execute([':type' => $_POST['type'],':id' => $_POST['who'],]);
 	$test = $stmt -> rowCount();
+	$stmt -> closeCursor();
 	if($test){
-		echo "Zmieniono typ użytkownika!";
+		echo "Zmieniono typ użytkownika! ;";
+		$stmt = $pdo -> prepare('SELECT user_modified FROM user WHERE user_id = :id;');
+		$stmt -> execute([':id' => $_POST['who'],]);
+		$row = $stmt -> fetch();
+		echo $row['user_modified'];
 	}else{
 		echo "Coś poszło nie tak!";
 	}
